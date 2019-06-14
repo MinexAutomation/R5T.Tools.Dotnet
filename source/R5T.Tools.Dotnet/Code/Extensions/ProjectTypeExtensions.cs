e1779f4ea7f21a580446f8917eb60b8921865cee
MinexAutomation <minex.automation@gmail.com>
2019-06-14 18:27:29 -0400

    Added ProjectType to DotnetNewProjectType mapping functionality.

---
+++ source/R5T.Tools.Dotnet/Code/Extensions/ProjectTypeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using R5T.Code.VisualStudio;
+
+
+namespace R5T.Tools.Dotnet
+{
+    public static class ProjectTypeExtensions
+    {
+        public static DotnetNewProjectType DotnetNewProjectType(this ProjectType projectType)
+        {
+            var dotnetNewProjectType = Utilities.GetDotnetNewProjectType(projectType);
+            return dotnetNewProjectType;
+        }
+    }
+}
---
+++ source/R5T.Tools.Dotnet/Code/Utilities.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using R5T.Code.VisualStudio;
+using R5T.NetStandard;
+
+
+namespace R5T.Tools.Dotnet
+{
+    public static class Utilities
+    {
+        public static DotnetNewProjectType GetDotnetNewProjectType(ProjectType projectType)
+        {
+            switch(projectType)
+            {
+                case ConsoleProjectType console:
+                    return DotnetNewProjectType.Console;
+
+                case LibraryProjectType library:
+                    return DotnetNewProjectType.Library;
+
+                case TestingProjectType testing:
+                    return DotnetNewProjectType.MSTest;
+
+                case UnspecifiedProjectType unspecified:
+                default:
+                    throw new ArgumentException(ExceptionHelper.UnexpectedTypeExceptionMessage(projectType));
+            }
+        }
+    }
+}
